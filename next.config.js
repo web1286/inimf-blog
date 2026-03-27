ba527133007799b6bc568e4e8c03eb72f783862c
web1286 <uinimf@gmail.com>
2026-03-27 19:54:17 +0800

    fix: update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  // GitHub Pages 部署时需要 basePath（仓库名）
+  // 绑定自定义域名后这里可以去掉
   basePath: isProd && process.env.GITHUB_PAGES ? '/inimf-blog' : '',
   assetPrefix: isProd && process.env.GITHUB_PAGES ? '/inimf-blog/' : '',
   images: {
